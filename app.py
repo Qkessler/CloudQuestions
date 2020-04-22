73e7718810fe19a79f85e9901c237e7d13cc4ab1
Qkessler <qkessler@gmail.com>
2020-04-22 14:17:14 +0200

    Got the answers but tabs are not the way to go

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ import re
 
 pat_headers = re.compile(r'## .*')
 pat_questions = re.compile(r'- .*')
+pat_answers = re.compile(r'\s{4}.*')
 
 
 def parsing_markdown():
     with open('tests/SSL TLS.md', 'r') as f:
         lines = f.readlines()
+
+    # Headers will be tables in the database.
     headers = [line for line in lines if pat_headers.match(line)]
-    # questions = pat_questions.findall(str(lines))
     questions = [line for line in lines if pat_questions.match(line)]
-    print([i.strip('\n') for i in questions])
-    # questions_stripped = []
-    # for q in questions:
-    #     if q != '\\n':
-    #         questions_stripped.append(q)
-    # print(questions_stripped)
+    questions_stripped = [q.strip('\n') for q in questions]
+    answers = [line for line in lines if pat_answers.match(line)]
+    print(answers)

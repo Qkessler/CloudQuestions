a4a1e4a85166f21b65c18133834705f04f1e6edf
Qkessler <qkessler@gmail.com>
2020-04-22 14:04:46 +0200

    Adding stripped questions

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import re
 
 pat_headers = re.compile(r'## .*')
+pat_questions = re.compile(r'- .*')
 
 
 def parsing_markdown():
     with open('tests/SSL TLS.md', 'r') as f:
         lines = f.readlines()
     headers = [line for line in lines if pat_headers.match(line)]
+    # questions = pat_questions.findall(str(lines))
+    questions = [line for line in lines if pat_questions.match(line)]
+    print([i.strip('\n') for i in questions])
+    # questions_stripped = []
+    # for q in questions:
+    #     if q != '\\n':
+    #         questions_stripped.append(q)
+    # print(questions_stripped)

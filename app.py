e7bfee4d018eb24d409cfb30a0a8534e1480e27d
Qkessler <qkessler@gmail.com>
2020-04-22 14:34:41 +0200

    Adding line_num

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ pat_questions = re.compile(r'- .*')
 pat_answers = re.compile(r'\s{4}.*')
 
 
+def line_num(line, _file):
+    with open(_file) as f:
+        for i, l in enumerate(f):
+            if line in l:
+                return i
+
+
 def parsing_markdown():
     with open('tests/SSL TLS.md', 'r') as f:
         lines = f.readlines()
@@ -14,4 +21,4 @@ def parsing_markdown():
     questions = [line for line in lines if pat_questions.match(line)]
     questions_stripped = [q.strip('\n') for q in questions]
     answers = [line for line in lines if pat_answers.match(line)]
-    print(answers)
+    

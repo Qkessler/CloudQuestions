abb6b2a03b273c2a138478715868c275e44dcb6d
Qkessler <qkessler@gmail.com>
2020-04-23 09:59:36 +0200

    Fixing second file bug

--- app.py
+++ app.py
@@ -12,24 +12,23 @@ def line_num(line, _file):
                 return i
 
 
-def parsing_markdown():
-    file = 'tests/SSL TLS.md'
+def parsing_markdown(file):
     return_files = {}
     with open(file, 'r') as f:
         file_lines = f.readlines()
     lines = []
     for line in file_lines:
-        if line != '\n':
-            line = line.strip('\n')
+        # if line != '\n':
+        #     line = line.strip('\n')
         lines.append(line)
-    # Headers will be tables in the database.
     headers = [line for line in lines if pat_headers.match(line)]
     return_files['headers'] = headers
     questions = [q.strip('\n') for q in lines
                  if pat_questions.match(q)]
     questions_num = [line_num(q, file) for q in questions]
+    print(questions_num)
     answers = []
-    for _ in questions_num:
+    for _ in questions_num[:9]:
         answer = []
         while pat_answers.match(lines[_+2]) or lines[_+2] == '\n':
             answer.append(lines[_+2].strip('    '))
--- program.py
+++ program.py
@@ -2,4 +2,5 @@ from app import parsing_markdown
 
 
 if __name__ == '__main__':
-    return_files = parsing_markdown()
+    return_files = parsing_markdown('tests/B4 2.md')
+    print(return_files)

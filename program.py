97d3a298203d928410dfa02246a04411e1655b04
Qkessler <qkessler@gmail.com>
2020-04-23 10:05:56 +0200

    Adding test

--- program.py
+++ program.py
@@ -1,6 +1,14 @@
 from app import parsing_markdown
+import random
 
 
 if __name__ == '__main__':
-    return_files = parsing_markdown('tests/B4 2.md')
-    print(return_files)
+    return_files = parsing_markdown('tests/SSL TLS.md')
+    q_a = return_files['q_a']
+    print(q_a)
+    random_q = random.randint(0, len(q_a.keys()))
+    print(random_q)
+    print(list(q_a.keys())[random_q])
+    key = input('Press whatever for answer: ')
+    if key:
+        print(list(q_a.values())[random_q])

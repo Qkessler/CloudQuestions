7bab359c7653913b24a90778d6dd851115b9dedb
Qkessler <qkessler@gmail.com>
2020-04-23 12:27:21 +0200

    fixing problem with long line

--- app.py
+++ app.py
@@ -14,36 +14,17 @@ def line_num(line, _file):
                 return i
 
 
-# def parsing_markdown(file):
-#     return_files = {}
-#     with open(file, 'r') as f:
-#         file_lines = f.readlines()
-#     final_line = finalline_file(file)
-#     lines = file_lines
-#     headers = [line for line in lines if pat_headers.match(line)]
-#     return_files['headers'] = headers
-#     questions = [q.strip('\n') for q in lines
-#                  if pat_questions.match(q)]
-#     questions_num = [line_num(q, file) for q in questions]
-#     answers = []
-#     for _ in questions_num:
-#         answer = []
-#         while (_ + 1 != final_line) and (pat_answers.match(lines[_+1])
-#                                          or lines[_+1] == '\n'):
-#             answer.append(lines[_+1].strip('    '))
-#             _ += 1
-#         answers.append("".join(answer))
-#     q_a = dict(zip(questions, answers))
-#     return_files['q_a'] = q_a
-#     return return_files
 def parsing_markdown(file):
+    return_file = {}
     with open(file, 'r') as f:
         file_lines = f.readlines()
     lines = file_lines
     questions = [q.strip('\n') for q in lines
                  if pat_questions.match(q)]
     q_a = get_inside(questions, file)
-    return q_a
+    return_file['q_a'] = q_a
+    return_file['file_name'] = 
+    return return_file
 
 
 def finalline_file(file):
@@ -71,6 +52,7 @@ def get_inside(mark, file):
         values.append("".join(v))
     inside = dict(zip(mark, values))
     return inside
+
 
 def random_question(q_a):
     random_q = random.randint(0, len(q_a.keys())-1)

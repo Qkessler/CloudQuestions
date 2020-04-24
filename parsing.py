6442c90ca922f3423bef70b1b7b0cb8de5a8c162
Qkessler <qkessler@gmail.com>
2020-04-24 18:37:19 +0200

    Adding comments on parsing files

--- parsing.py
+++ parsing.py
@@ -5,19 +5,21 @@ from models.topic import Topic
 from data import session_factory
 from question_service import same_questions
 
-
+# Patterns for parsing the markdown file.
 pat_headers = re.compile(r'## .*')
 pat_questions = re.compile(r'- .*')
 pat_answers = re.compile(r'\s{4}.*')
 
 
-def line_num(line, _file):
+# Gets the line number of the line that contains the string given.
+def line_num(string, _file):
     with open(_file) as f:
         for i, l in enumerate(f):
-            if line in l:
+            if string in l:
                 return i
 
 
+# Scrubs the name given to make sure it's compatible in the db.
 def scrub_name(name):
     chars = []
     for c in name:
@@ -28,6 +30,9 @@ def scrub_name(name):
     return "".join(chars)
 
 
+# Given a file as parameter, reads the lines and gets the questions,
+# being the markdown toggles. After that, parses for each question,
+# the answer below.
 def parsing_markdown(file):
     return_file = {}
     with open(file, 'r') as f:
@@ -42,7 +47,7 @@ def parsing_markdown(file):
     s_name = scrub_name(file_name)
     return_file['file_name'] = s_name
 
-    # Getting the questions and answers in the db.
+    # Inserting the questions and answers in the db.
     session = session_factory.create_session()
     for q, a in q_a.items():
         if not same_questions(q, s_name):
@@ -57,8 +62,10 @@ def parsing_markdown(file):
     return return_file
 
 
-def get_inside(mark, file):
-    mark_num = [line_num(q, file) for q in mark]
+# Given a list of questions, we calculate the line numbers and add to
+# the dictionary all that matches the answer pattern (4 spaces).
+def get_inside(question_list, file):
+    question_num = [line_num(q, file) for q in question_list]
     with open(file, 'r') as f:
         lines = f.readlines()
     clean_lines = []
@@ -68,7 +75,7 @@ def get_inside(mark, file):
             line.strip('\n')
         clean_lines.append(line)
     values = []
-    for _ in mark_num:
+    for _ in question_num:
         v = []
         not_last = True
         while not_last and (pat_answers.match(clean_lines[_+1])
@@ -77,16 +84,18 @@ def get_inside(mark, file):
             v.append(clean_lines[_+1].strip('    '))
             _ += 1
         values.append("".join(v))
-    inside = dict(zip(mark, values))
+    inside = dict(zip(question_list, values))
     return inside
 
 
+# Basic print function for a dict.
 def print_q_a(q_a):
     for key, value in q_a.items():
         print(f'{key}:')
         print(f'{value}')
 
 
-def html_translator(answer):
-    html = markdown.markdown(answer)
+# Html translator given a string.
+def html_translator(string):
+    html = markdown.markdown(string)
     print(html)

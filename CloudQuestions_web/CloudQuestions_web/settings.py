e6123322b1c5625df1864aa7c5581b22d5544732
Qkessler <qkessler@gmail.com>
2020-04-27 14:14:41 +0200

    Modified settings

--- CloudQuestions_web/CloudQuestions_web/settings.py
+++ CloudQuestions_web/CloudQuestions_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sqlalchemy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,6 +74,8 @@ WSGI_APPLICATION = 'CloudQuestions_web.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+
 
 DATABASES = {
     'default': {

ea8d56a310016624f9ccb3c8a644d31011d5c5cc
Qkessler <qkessler@gmail.com>
2020-04-29 14:13:24 +0200

    Getting almost good enough file uploading

--- CloudQuestions_web/questions/urls.py
+++ CloudQuestions_web/questions/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'questions'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:topic>/', views.detail, name='detail'),
-    path('upload_file/', views.upload_file, name='upload_file')
+    path('<str:topic>/', views.detail, name='detail')
 ]
--- CloudQuestions_web/questions/views.py
+++ CloudQuestions_web/questions/views.py
@@ -1,25 +1,32 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import SearchForm, UploadFileForm
 from questions.src import question_service, parsing
-import pdb
 
 
 def index(request):
     context = {}
+    correct_upload = ''
     if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            topic = form.cleaned_data.get('search_text')
+        search_form = SearchForm(request.POST)
+        upload_file_form = UploadFileForm(request.POST)
+        if search_form.is_valid():
+            topic = search_form.cleaned_data.get('search_text')
             topics = question_service.search_engine(topic)
             db_topics = []
             for t in topics:
                 db_topics.append(parsing.scrub_name(t))
             topics_return = dict(zip(topics, db_topics))
             context['topics_return'] = topics_return
+        elif upload_file_form.is_valid():
+            uploaded = request.FILES.get('file')
+            correct_upload = 'File uploaded correctly'
     else:
-        form = SearchForm()
-    context['form'] = form
+        search_form = SearchForm()
+        upload_file_form = UploadFileForm()
+    context['search_form'] = search_form
+    context['upload_file_form'] = upload_file_form
+    context['correct_upload'] = correct_upload
     return render(request, 'questions/index.html', context)
 
 
@@ -28,20 +35,3 @@ def detail(request, topic):
     return render(request, 'questions/detail.html',
                   {'topic': topic,
                    'questions_by_topic': questions_by_topic})
-
-
-def upload_file(request):
-    # context = {}
-    # if request.method == 'POST':
-    #     # form = UploadFileForm(request.POST, request.FILES)
-    #     # if form.is_valid():
-    #     #     context['return_form'] = 'FORM IS VALID'
-    #     #     context['file'] = request.FILES['file']
-    #     # else:
-    #     #     context['return_form'] = 'FROM IS INVALID'
-    #     uploaded_file = request.FILES.get('file')
-    #     print(uploaded_file)
-    # else:
-    #     form = UploadFileForm()
-    # context['form'] = form
-    return render(request, 'questions/file.html')

1c4c78a3c5f4445780f09bb88b06e393ad6048aa
Qkessler <qkessler@gmail.com>
2020-05-04 19:57:22 +0200

    Adding start of transition

--- CloudQuestions_web/CloudQuestions_web/settings.py
+++ CloudQuestions_web/CloudQuestions_web/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'questions.apps.QuestionsConfig',
-    'star_ratings'
+    'django_sorcery',
+    'questions.apps.QuestionsConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,15 +75,17 @@ WSGI_APPLICATION = 'CloudQuestions_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'questions.sqlite'),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
---
+++ CloudQuestions_web/questions/models.py
@@ -0,0 +1,59 @@
+from django_sorcery.db import databases
+from datetime import datetime
+from django.contrib.auth.models import update_last_login, user_logged_in
+from django.contrib.auth import check_password, make_password
+from django.contrib.auth import random_characters
+
+
+db = databases.get('default')
+
+
+class Topic(db.Model):
+    __tablename__ = 'Topic'
+
+    id = db.Column(db.Integer(), primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String(), unique=True)
+    created = db.Column(db.DateTime,
+                        default=datetime.now, nullable=False)
+
+
+class Question(db.Model):
+    __tablename__ = 'Question'
+
+    id = db.Column(db.Integer(), primary_key=True,
+                   autoincrement=True, nullable=False)
+    created = db.Column(db.DateTime(), default=datetime.now,
+                        nullable=False)
+    topic = db.Column(db.Integer(),
+                      db.ForeignKey(Topic.id), nullable=False)
+    question = db.Column(db.String(), unique=True)
+    answer = db.Column(db.String())
+
+
+user_logged_in.disconnect(update_last_login)
+
+
+class User(db.Model):
+    __tablename__ = "User"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(), unique=True)
+    salt = db.Column(db.String(length=10))
+    password = db.Column(db.String(legth=128))
+
+    USERNAME_FIELD = 'username'
+
+    def is_authenticated(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def set_password(self, password):
+        if not self.salt:
+            self.salt = random_characters(10)
+        self.password = make_password(password, salt=self.salt)

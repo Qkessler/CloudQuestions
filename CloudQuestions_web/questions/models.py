c26396c13b442568067ce60d57a0e4984a329768
Qkessler <qkessler@gmail.com>
2020-05-14 13:56:19 +0200

    Think gonna start using vanilla django orm.

--- CloudQuestions_web/questions/models.py
+++ CloudQuestions_web/questions/models.py
@@ -2,31 +2,11 @@ from django_sorcery.db import databases
 from django.conf import settings
 from datetime import datetime
 from django.contrib.auth.models import User as auth_user
-from sqlalchemy.orm import mapper
+from django.contrib.auth.models import UserManager
+from sqlalchemy.orm import relationship
 
 
 db = databases.get('default')
-
-
-class User(object):
-    def __init__(self, id, password, last_login, is_superuser,
-                 username, first_name, email, is_staff, is_active,
-                 date_joined, last_name):
-        self.id = id
-        self.password = password
-        self.last_login = last_login
-        self.is_superuser = is_superuser
-        self.username = username
-        self.first_name = first_name
-        self.email = email
-        self.is_staff = is_staff
-        self.is_active = is_active
-        self.date_joined = date_joined
-        self.last_name = last_name
-
-        
-mapper(User, auth_user)
-
 
 class Topic(db.Model):
     __tablename__ = 'Topic'
@@ -54,7 +34,7 @@ class Question(db.Model):
 class Rating(db.Model):
     id = db.Column(db.Integer(), primary_key=True,
                    autoincrement=True, nullable=False)
-    user = db.Column(db.ForeignKey(User.id), nullable=False, index=True)
+    # user = db.Column(db.ForeignKey(settings.AUTH_USER_MODEL), nullable=False, index = True)
     topic = db.Column(db.Integer(),
                       db.ForeignKey(Topic.id), nullable=False)
     created = db.Column(db.DateTime(), default=datetime.now,

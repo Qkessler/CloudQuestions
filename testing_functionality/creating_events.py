a020775530268d917894337a2f8499aad5734fee
Qkessler <qkessler@gmail.com>
2020-05-22 14:04:49 +0200

    Correct event creation function.

--- testing_functionality/creating_events.py
+++ testing_functionality/creating_events.py
@@ -1,7 +1,9 @@
-import datetime
+from datetime import datetime, timedelta
 from pprint import pprint as pp
+import random
 
 
+DEFAULT_URL = 'http://127.0.0.1:8000/'
 day_colors = {
     'red': 1,
     'yellow': 3,
@@ -11,21 +13,24 @@ day_colors = {
 
 def create_event(topic, color):
     event_colors = [str(num+1) for num in range(11)]
+    date = datetime.now().date() + timedelta(days=day_colors.get(color))
+    description = 'This is an event created by CloudQuestions as '
+    description += f'a reminder to revisit the topic "{topic}" '
+    description += 'in the following link: \n'
+    decription = description + DEFAULT_URL + 'questions/' + topic
     event = {
         'summary': 'AR ' + topic,
-        'description': 'This is an event created by CloudQuestions',
+        'description': description,
         'start': {
-            'dateTime': '2015-05-28T09:00:00-07:00',
-            'timeZone': 'America/Los_Angeles',
+            'date': str(date),
         },
         'end': {
-            'dateTime': '2015-05-28T17:00:00-07:00',
-            'timeZone': 'America/Los_Angeles',
+            'date': str(date),
         },
+        'colorId': event_colors[random.randint(0, len(event_colors) - 1)],
         'reminders': {
             'useDefault': False,
             'overrides': [
-                {'method': 'email', 'minutes': 24 * 60},
                 {'method': 'popup', 'minutes': 10},
             ],
         },
@@ -36,7 +41,7 @@ def create_event(topic, color):
 
 
 def main():
-    pp(create_event('TEST', 'red'))
+    pp(create_event('TEST', 'green'))
 
 
 if __name__ == '__main__':

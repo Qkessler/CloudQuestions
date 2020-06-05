0be52cac2e0030b703288cc6349f40429edbda3c
Qkessler <qkessler@gmail.com>
2020-06-05 17:16:54 +0200

    Trying to get cool file forms.

--- CloudQuestions_web/questions/forms.py
+++ CloudQuestions_web/questions/forms.py
@@ -14,8 +14,7 @@ class UploadFileForm(forms.Form):
     file = forms.FileField(validators=[validators.validate_markdown],
                            label="",
                            widget=forms.FileInput(
-                               attrs={'class': 'custom-file-input',
-                                      'id': 'file_upload'}))
+                               attrs={'id': 'file_upload'}))
 
 
 class CreateTopicForm(forms.Form):

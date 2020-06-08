ebdac50e586154d6e88d9b174a0e0bf6dcac4d83
Qkessler <qkessler@gmail.com>
2020-06-08 18:53:39 +0200

    Fixing the url for help, create topic working again.

--- CloudQuestions_web/questions/urls.py
+++ CloudQuestions_web/questions/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = 'questions'
 urlpatterns = [
     path('', views.questions, name='questions'),
-    path('<str:toggle_help>', views.questions, name='questions'),
     path('create_topic', views.create_topic, name='create_topic'),
     path('create_topic/<str:topic_added>',
          views.create_topic, name='create_topic'),
     path('<str:topic>/', views.detail, name='detail'),
     path('<str:topic>/random/', views.random_questions, name='random'),
     path('<str:topic>/random/<str:list_questions>',
-         views.random_questions, name='random')
+         views.random_questions, name='random'),
+    path('<str:toggle_help>', views.questions, name='questions')
 ]
--- CloudQuestions_web/questions/views.py
+++ CloudQuestions_web/questions/views.py
@@ -16,14 +16,15 @@ def questions(request, toggle_help=None):
     context['searched'] = False
     context['empty'] = True
     context['help'] = 'help_false'
-    if request.GET.get('upload_topic'):
-        return redirect('questions:create_topic')
+    print(toggle_help)
     if toggle_help:
         context['help'] = 'help_true'
     if request.GET.get('toggle_help'):
         if toggle_help == "true":
             return redirect('questions:questions')
         return redirect('questions:questions', 'true')
+    if request.GET.get('upload_topic'):
+        return redirect('questions:create_topic')
     if request.method == 'POST':
         search_form = SearchForm(prefix='search_form')
         upload_file_form = UploadFileForm(prefix='upload_file_form')

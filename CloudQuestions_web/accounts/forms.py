7b688d0782638a60b8f283b1960df8837f10cdf4
Qkessler <qkessler@gmail.com>
2020-07-01 20:28:21 +0200

    Fixing register form, getting crispy forms helper.

--- CloudQuestions_web/accounts/forms.py
+++ CloudQuestions_web/accounts/forms.py
@@ -3,13 +3,30 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from captcha.fields import ReCaptchaField
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.layout import (Layout, Fieldset, ButtonHolder,
+                                 Submit, Field, Div)
 
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=200, required=True)
-    captcha = ReCaptchaField()
+    captcha = ReCaptchaField(label='')
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+                '',
+                Div(
+                    'username',
+                    'email',
+                    'password1',
+                    'password2',
+                    'captcha',
+                    css_id="register-form")),
+            ButtonHolder(
+                Submit('submit', 'Signup', css_class='button white')
+            )
+        )
